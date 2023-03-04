64d924db2d21ff4cb925f078832d674d1609f250
LordGum <dari.vision@mail.ru>
2023-03-04 14:17:36 +0300

    commit 7
    уже готовый вариант, но без progressLine

--- app/src/main/java/com/example/audiorecorder1/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder1/MainActivity.java
@@ -260,4 +260,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+
 }
--- app/src/main/java/com/example/audiorecorder1/ScrollingActivity.java
+++ app/src/main/java/com/example/audiorecorder1/ScrollingActivity.java
@@ -2,7 +2,10 @@ package com.example.audiorecorder1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,11 +24,12 @@ public class ScrollingActivity extends AppCompatActivity {
 
     private TextView songName,currentTime, durationTime;
     private ImageView pauseImage, nextImage, previousImage;
+    private ImageView gifImage, noteImage;
     private SeekBar seekBar;
 
     private int songPosition;
     static MediaPlayer mediaPlayer;
-    List<Record> recordList;
+    private List<Record> recordList;
 
     private RecordDatabase recordDatabase;
 
@@ -42,7 +46,13 @@ public class ScrollingActivity extends AppCompatActivity {
         }
 
         setResources_startMediaPlayer();
+        startAnimation();
+
         clickPlayButton();
+        songEnd_GoToNext();
+        clickNextButton();
+        clickPreviousButton();
+
     }
 
     private void init() {
@@ -54,9 +64,11 @@ public class ScrollingActivity extends AppCompatActivity {
         nextImage = findViewById(R.id.nextImage);
         previousImage = findViewById(R.id.previousImage);
 
-        seekBar = findViewById(R.id.seekBar);
+        gifImage = findViewById(R.id.gifImage);
+        noteImage = findViewById(R.id.noteImage);
 
 
+        //ниже получаю recordList из базы данных
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -68,7 +80,6 @@ public class ScrollingActivity extends AppCompatActivity {
             }
         });
         thread.start();
-
         try {
             thread.join();
         } catch (InterruptedException e) {
@@ -98,6 +109,8 @@ public class ScrollingActivity extends AppCompatActivity {
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
         mediaPlayer.start();
+
+        workWithSeekbar();
     }
 
     private void clickPlayButton() {
@@ -135,5 +148,133 @@ public class ScrollingActivity extends AppCompatActivity {
         else {
             return hour + ":" + min + ":" + sec;
         }
+    }
+
+    private void startAnimation() {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(gifImage, "rotation", 0f, 360f);
+        animator.setDuration(1000);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(noteImage, "rotation", 0f, 360f);
+        animator1.setDuration(1000);
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playTogether(animator, animator1);
+        animatorSet.start();
+    }
+
+    private void clickNextButton() {
+        nextImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                songPosition = ( (songPosition+1)%(recordList.size()) );
+                Record record1 = recordList.get(songPosition);
+                File file1 = new File(record1.getRecPath());
+                Uri uri1 = Uri.parse(file1.toString());
+
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri1);
+                mediaPlayer.start();
+
+                songName.setText(record1.getRecName());
+                Long time = Long.parseLong(record1.getRecDuration());
+                durationTime.setText(durationTime(time));
+
+                pauseImage.setBackgroundResource(R.drawable.ic_pause_scrolling_activity);
+                startAnimation();
+            }
+        });
+    }
+
+    private void clickPreviousButton() {
+        previousImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+
+                songPosition = ( (songPosition-1)<0 ) ? (recordList.size()-1) : (songPosition-1);
+                Record record2 = recordList.get(songPosition);
+                File file2 = new File(record2.getRecPath());
+                Uri uri2 = Uri.parse(file2.toString());
+
+                mediaPlayer = MediaPlayer.create(getApplicationContext(), uri2);
+                mediaPlayer.start();
+
+                songName.setText(record2.getRecName());
+                Long time = Long.parseLong(record2.getRecDuration());
+                durationTime.setText(durationTime(time));
+
+                pauseImage.setBackgroundResource(R.drawable.ic_pause_scrolling_activity);
+                startAnimation();
+            }
+        });
+    }
+
+    private void songEnd_GoToNext() {
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                //nextImage.performClick();
+                pauseImage.setBackgroundResource(R.drawable.ic_play_scrolling_activity);
+                mediaPlayer.pause();
+            }
+        });
+    }
+
+    private void workWithSeekbar() {
+        //ниже работаю с seekBar,те обновляю
+        seekBar = findViewById(R.id.seekBar);
+        Thread updateSeekbar;
+
+        updateSeekbar = new Thread() {
+            @Override
+            public void run() {
+                int totalDuration = mediaPlayer.getDuration();
+                int currentTimePosition = 0;
+
+                while (currentTimePosition < totalDuration) {
+                    try{
+                        sleep(500);
+                        currentTimePosition = mediaPlayer.getCurrentPosition();
+                        seekBar.setProgress(currentTimePosition);
+                    } catch (InterruptedException | IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        seekBar.setMax(mediaPlayer.getDuration());
+        updateSeekbar.start();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+            }
+        });
+
+        //обновляю время
+        final Handler handler = new Handler();
+        final int delay = 1000;
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Long time = Long.parseLong( String.valueOf(mediaPlayer.getCurrentPosition()));
+                String currentTimeValue = durationTime(time);
+                currentTime.setText(currentTimeValue);
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
     }
 }
